fe82ab587fea32a474fc084644f8406e94a0ca0a
yangnk <yangningkai@qq.com>
2021-12-04 22:14:03 +0800

    新增springbootDemo

--- springBootDemo/src/main/java/com/yangnk/springBootDemo/SpringBootDemoApplication.java
+++ springBootDemo/src/main/java/com/yangnk/springBootDemo/SpringBootDemoApplication.java
@@ -1,20 +1,13 @@
-package com.yangnk;
+package com.yangnk.springBootDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author yangnk
- * @version 1.0.0
- * @date 2019-04-24
- */
 @SpringBootApplication
-public class Application {
+public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
-
-		SpringApplication.run(Application.class, args);
-
+		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
 
 }
