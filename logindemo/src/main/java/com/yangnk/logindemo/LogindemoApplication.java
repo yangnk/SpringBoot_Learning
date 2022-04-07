6a9d7e4a74e80a58d409d9d6a2dd41626dcc0697
yangnk <yangningkai@qq.com>
2022-04-07 12:04:09 +0800

    更新

--- logindemo/src/main/java/com/yangnk/logindemo/LogindemoApplication.java
+++ logindemo/src/main/java/com/yangnk/logindemo/LogindemoApplication.java
@@ -1,16 +1,13 @@
-package com.yangnk.simpleMQ;
+package com.yangnk.logindemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author yangnk
- * @version 1.0.0
- * @date 2019-04-24
- */
 @SpringBootApplication
-public class RabbitMQApplication {
+public class LogindemoApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(RabbitMQApplication.class, args);
+		SpringApplication.run(LogindemoApplication.class, args);
 	}
+
 }
